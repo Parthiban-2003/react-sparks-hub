c1cd210ac5930bec5b306a8240f3e398b875739b
ParthibanR <parthibanvijay467@gmail.com>
2026-01-31 19:52:26 +0530

    MODIFIED: SearchFilter modified

--- react-sparks/src/searchFilter/searchFilter.jsx
+++ react-sparks/src/searchFilter/searchFilter.jsx
@@ -1,21 +1,20 @@
 import { useState } from "react";
 
-function SearchFilter()
-{
-    const SearchLists = ['HTML','CSS','Bootstrap','JavaScript','ReactJS','JQuery'];
+function SearchFilter() {
+    const SearchLists = ['HTML', 'CSS', 'Bootstrap', 'JavaScript', 'ReactJS', 'JQuery'];
     const [search, setSearch] = useState('');
-    return(
+    return (
         <>
             <h1>Input Search Filter Displays:</h1>
             <input type="text" placeholder="Type Anything..." value={search}
-            onChange={(e)=>setSearch(e.target.value)} />
+                onChange={(e) => setSearch(e.target.value)} />
 
             <ul>
                 {
-                    SearchLists.filter((item)=>item.toLowerCase().includes(search.toLowerCase()))
-                    .map((item)=>{
-                        return <li key={item}>{item}</li>
-                    })
+                    SearchLists.filter((item) => item.toLowerCase().includes(search.toLowerCase()))
+                        .map((item) => {
+                            return <li key={item}>{item}</li>
+                        })
                 }
             </ul>
         </>
